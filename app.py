7e1a51e3662f4b1af03a4e5aafa8c1874ddd9889
al <alanseman@hotmail.com>
2022-03-03 16:51:16 +0100

    docs: documentation du fichier principal

--- app.py
+++ app.py
@@ -1,6 +1,14 @@
+    """_summary_
+    projet de demo pour la création d'un CD
+
+    Returns:
+        _type_: _description_
+    """
+
 from flask import Flask
 import os
 
+# Recuperation des ENVs
 PORT = os.environ.get("PORT", "80")
 HOST = os.environ.get("HOST", "0.0.0.0")
 
@@ -8,7 +16,14 @@ app = Flask("demo")
 
 @app.get("/")
 def hello():
+    
+    """
+    Fonction appelee pour le chemin par defaut.
+    Returns:
+        str: un message :
+    """
     return "hello world!"
+
 
 if __name__ == "__main__":
     app.run(host="HOST", port=PORT)
